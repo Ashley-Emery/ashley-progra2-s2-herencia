e0210eb9e867a557f1c883ec09363991c9ad78fc
Ashley Fajardo <a.e.fajardo06@gmail.com>
2026-04-28 14:55:14 -0600

    initial commit

---
+++ src/ashley/progra2/s2/herencia/AshleyProgra2S2Herencia.java
@@ -0,0 +1,20 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Main.java to edit this template
+ */
+package ashley.progra2.s2.herencia;
+
+/**
+ *
+ * @author ashley
+ */
+public class AshleyProgra2S2Herencia {
+
+    /**
+     * @param args the command line arguments
+     */
+    public static void main(String[] args) {
+        // TODO code application logic here
+    }
+    
+}
